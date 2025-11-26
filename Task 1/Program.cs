62ea6ec81e4bdb33bf23ebbfd4c9152dd78df7d0
Roshna-karki <xettrirsohna12@gmail.com>
2025-11-26 11:23:27 +0545

    first commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount("12345084023472034", 1000);
+
+        Console.WriteLine($"Account Number: {account.AccountNumber}");
+        Console.WriteLine($"Opening Balance: {account.Balance}");
+
+        account.Deposit(500000);
+        account.Withdraw(30000);
+
+        Console.WriteLine($"Remaining Balance: {account.Balance}");
+    }
+}
